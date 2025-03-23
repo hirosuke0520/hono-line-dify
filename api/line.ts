1eedaf997d9293314f4c660ff2ff45dff94e6a75
hirosuke0520 <hirosuke.creator@gmail.com>
2025-03-24 03:50:29 +0900

    機密情報を環境変数に置き換え

--- api/line.ts
+++ api/line.ts
@@ -1,8 +1,6 @@
-import { LINE_CHANNEL_ACCESS_TOKEN } from "../const.js"
-
 export const getContentByMessageId = async (messageId: string): Promise<Blob> => {
   const res = await fetch(`https://api-data.line.me/v2/bot/message/${messageId}/content`, {
-    headers: { Authorization: `Bearer ${LINE_CHANNEL_ACCESS_TOKEN}` }
+    headers: { Authorization: `Bearer ${process.env.LINE_CHANNEL_ACCESS_TOKEN}` }
   })
   return await res.blob()
 }
