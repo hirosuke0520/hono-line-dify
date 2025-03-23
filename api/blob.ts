16ecd3acf63816426966261e1b81a932c5db9522
hirosuke0520 <hirosuke.creator@gmail.com>
2025-03-24 03:13:32 +0900

    Blob削除関数にtoken持たせる

--- api/blob.ts
+++ api/blob.ts
@@ -1,7 +1,5 @@
 import { del, put } from "@vercel/blob"
 
-import { extractExtensionFromContentType } from "../utils.js"
-
 export const uploadToBlobStorage = async (messageId: string, extension: string, blob: Blob) => {
   try {
     const { url } = await put(`dify/tmp/${messageId}.${extension}`, blob, {
@@ -15,7 +13,7 @@ export const uploadToBlobStorage = async (messageId: string, extension: string, 
 
 export const deleteStorageFile = async (messageId: string, extension: string): Promise<void> => {
   try {
-    await del(`dify/tmp/${messageId}.${extension}`)
+    await del(`dify/tmp/${messageId}.${extension}`, { token: process.env.BLOB_READ_WRITE_TOKEN })
   } catch (error) {
     console.log(error)
   }
